544b141e08dbf4f60ad07985232ffd399ee04af4
truongvanquocvu <quocvu010495@gmail.com>
2022-09-15 16:01:46 +0700

    modun1

---
+++ baiso7/hdcdtt.js
@@ -0,0 +1,15 @@
+function check(){
+let num1=Number(document.getElementById("number").value);
+let num2=Number(document.getElementById("n1").value);
+let num3=Number(document.getElementById("n2").value);
+let kq ='';
+if (num2 == "VND" && num3 == "VND") {
+    kq= num1; 
+} else { 
+    if ( num2)
+}
+document.getElementById("Result").innerHTML = kq;     
+
+
+
+}
---
+++ baiso7/ml.js
@@ -0,0 +1,33 @@
+function Addtion(){
+
+let num1=Number(document.getElementById('number1').value);
+let num2=Number(document.getElementById('number2').value);
+let kq= num1+num2;
+document.getElementById('result').innerHTML="Ket Qua La: " + kq;
+
+}
+function ba(){
+
+    let num1=Number(document.getElementById('number1').value);
+    let num2=Number(document.getElementById('number2').value);
+    let kq= num1-num2;
+    document.getElementById('result').innerHTML="Ket Qua La: " + kq;
+    
+    }
+ function me(){
+
+        let num1=Number(document.getElementById('number1').value);
+        let num2=Number(document.getElementById('number2').value);
+        let kq= num1*num2;
+        document.getElementById('result').innerHTML="Ket Qua La: " + kq;
+        
+        }
+function con(){
+
+            let num1=Number(document.getElementById('number1').value);
+            let num2=Number(document.getElementById('number2').value);
+            let kq= num1/num2;
+            document.getElementById('result').innerHTML="Ket Qua La: " +kq;
+            
+            }
+                        
