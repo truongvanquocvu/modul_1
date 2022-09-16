2b465ba023f0eb52d8c2abf775b1c3e585de26ed
truongvanquocvu <quocvu010495@gmail.com>
2022-09-16 08:14:03 +0700

    baitap

--- baiso7/hdcdtt.js
+++ baiso7/hdcdtt.js
@@ -1,15 +1,25 @@
-function check(){
-let num1=Number(document.getElementById("number").value);
-let num2=Number(document.getElementById("n1").value);
-let num3=Number(document.getElementById("n2").value);
-let kq ='';
-if (num2 == "VND" && num3 == "VND") {
-    kq= num1; 
-} else { 
-    if ( num2)
-}
-document.getElementById("Result").innerHTML = kq;     
-
-
-
+function check() {
+    let num1 = Number(document.getElementById("number").value);
+    let num2 = Number(document.getElementById("n1").value);
+    let num3 = Number(document.getElementById("n2").value);
+    let kq = "";
+    // if (num2 == "VND" && num3 == "VND") {
+    //     kq= num1; 
+    // } else if (num2 == "VND" && num3 == "USD") {
+    //     kq=num1/24000;
+    // } else if (num2 == "USD" && num3 == "USD") {
+    //     kq=num1
+    // } { 
+    //     else if (num2 == "USD" && num3 == "VND")
+    // }     kq=num1*24000;
+    if (num2 == "VND" && num3 == "VND") {
+        kq = num1;
+    } else if (num2 == "VND" && num3 == "USD") {
+        kq = num1 / 24000;
+    } else if (num2 == "USD" && num3 == "USD") {
+        kq = num1;
+    } else if (num2 == "USD" && num3 == "VND") {
+        kq = num1 * 24000;
+    }
+    document.getElementById("result2") = kq;
 }
